3fbeb37d2af7079966c732c4cfdeae61260edc20
jojepe <joaop.mendes@icloud.com>
2025-05-30 17:51:56 -0300

    Mudanças Fonsi

--- Cinefile/Views/ContentView.swift
+++ Cinefile/Views/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    init() {
+        UITabBar.appearance().backgroundColor = .white
+        }
     var body: some View {
                 
         TabView {
@@ -21,14 +24,10 @@ struct ContentView: View {
             }
             
             Tab("Perfil", systemImage: "person.circle") {
-                ProfileView(movie: Movie(poster: "land", title: "La La Land", year: "2017", synopsis: "O pianista Sebastian conhece a atriz Mia, e os dois se apaixonam perdidamente. Em busca de oportunidades para suas carreiras na competitiva Los Angeles, os jovens tentam fazer o relacionamento amoroso dar certo, enquanto perseguem fama e sucesso.", director: "Damien Chazelle", writers: "Damien Chazelle", isFavorite: false, isWatched: false, rating: 0))
+                ProfileView()
             }
         }
         .tint(.menta)
-        .toolbarBackground(
-            .thinMaterial,
-            for: .tabBar
-        )
     }
 }
 
