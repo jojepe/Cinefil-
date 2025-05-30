e1abebd560c031b6ab819aa6ba29ceaa6c095acd
jojepe <joaop.mendes@icloud.com>
2025-05-30 14:56:04 -0300

    Navigation entre telas

---
+++ Cinefile/Views/CardMovieView.swift
@@ -0,0 +1,39 @@
+//
+//  CardMovieView.swift
+//  Cinefile
+//
+//  Created by Joje on 30/05/25.
+//
+
+import SwiftUI
+
+struct CardMovieView: View {
+    
+    let movie: Movie
+    
+    var body: some View {
+        VStack(spacing: 2) { // Define o espaçamento vertical entre a Imagem e o Texto
+            Image(movie.poster)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 120, height: 180) // Altura fixa para a imagem
+            
+            Text(movie.title)
+                .font(.caption2)
+                .fontWeight(.medium)
+                .lineLimit(2) // Limita o texto a no máximo 2 linhas
+                .multilineTextAlignment(.leading)
+                .frame(width: 120, height: 35, alignment: .topLeading)
+                .foregroundStyle(.white)
+            
+        }
+        // Defina uma ALTURA TOTAL FIXA para o VStack (célula da grade)
+        // Calcule essa altura: altura_imagem + spacing_interno + altura_texto
+        // Ex: 150 (imagem) + 8 (spacing) + 35 (texto) = 193
+        .frame(width: 100, height: 190)
+    }
+}
+
+#Preview {
+    CardMovieView(movie: Movie(poster: "monica", title: "Turma Da Mônica: Uma Aventura no Tempo", year: "2007", synopsis: "Franjinha está construindo uma máquina do tempo e precisa reunir moléculas dos quatro elementos: ar, água, fogo e terra. Mônica joga Sansão e, sem querer, acerta o aparelho, espalhando os elementos. Agora a turma precisa consertá-la.", director: "Maurício de Souza", writers: "Mauricio de Sousa, Flávio de Souza, Didi Oliveira, Emerson Bernardo de Abreu", isFavorite: false, isWatched: false, rating: 0))
+}
--- Cinefile/Views/CatalogView.swift
+++ Cinefile/Views/CatalogView.swift
@@ -16,36 +16,32 @@ struct CatalogView: View {
     ]
     
     var body: some View {
-        ScrollView {
-            VStack {
-                Image("logo")
-                LazyVGrid(columns: columns, spacing: 30) {
-                    ForEach(dataModel.filmLists, id: \.title) { movie in
-                        VStack(spacing: 2) { // Define o espaçamento vertical entre a Imagem e o Texto
-                            Image(movie.poster)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 120, height: 180) // Altura fixa para a imagem
+        NavigationStack{
+            
+            ScrollView {
+                
+                VStack {
+                    
+                    Image("logo")
+                    
+                    LazyVGrid(columns: columns, spacing: 30) {
+                        ForEach(dataModel.filmLists, id: \.title) { movie in
                             
-                            Text(movie.title)
-                                .font(.caption2)
-                                .fontWeight(.medium)
-                                .lineLimit(2) // Limita o texto a no máximo 2 linhas
-                                .multilineTextAlignment(.leading)
-                                .frame(width: 120, height: 35, alignment: .topLeading)
-                                .foregroundStyle(.white)
-                            
+                            NavigationLink{
+                                
+                                MovieDetailView(movie: movie)
+                                
+                            } label:{
+                                CardMovieView(movie: movie)
+                                
+                            }
                         }
-                        // Defina uma ALTURA TOTAL FIXA para o VStack (célula da grade)
-                        // Calcule essa altura: altura_imagem + spacing_interno + altura_texto
-                        // Ex: 150 (imagem) + 8 (spacing) + 35 (texto) = 193
-                        .frame(width: 100, height: 190)
                     }
+                    .padding(5)
                 }
-                .padding(5)
+                .background(LinearGradient(gradient: Gradient(colors: [.fundoEscuro, .fundoClaro]), startPoint: .top, endPoint: .bottom))
+                .frame(maxWidth: .infinity)
             }
-            .background(LinearGradient(gradient: Gradient(colors: [.fundoEscuro, .fundoClaro]), startPoint: .top, endPoint: .bottom))
-            .frame(maxWidth: .infinity)
         }
     }
 }
