c5ee2ea2c7bc657e5b2763695c4d592bd5162a82
jojepe <joaop.mendes@icloud.com>
2025-05-30 15:52:01 -0300

    Tabbar

--- Cinefile/Views/ContentView.swift
+++ Cinefile/Views/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                
+        TabView {
+            
+            Tab("Players", systemImage: "film.stack"){
+                CatalogView()
+            }
+            
+            Tab("Contatos", systemImage: "magnifyingglass") {
+                CardMovieView(movie: Movie(poster: "land", title: "La La Land", year: "2017", synopsis: "O pianista Sebastian conhece a atriz Mia, e os dois se apaixonam perdidamente. Em busca de oportunidades para suas carreiras na competitiva Los Angeles, os jovens tentam fazer o relacionamento amoroso dar certo, enquanto perseguem fama e sucesso.", director: "Damien Chazelle", writers: "Damien Chazelle", isFavorite: false, isWatched: false, rating: 0))
+            }
+            
+            Tab("Perfil", systemImage: "person.circle") {
+                ProfileView(movie: Movie(poster: "land", title: "La La Land", year: "2017", synopsis: "O pianista Sebastian conhece a atriz Mia, e os dois se apaixonam perdidamente. Em busca de oportunidades para suas carreiras na competitiva Los Angeles, os jovens tentam fazer o relacionamento amoroso dar certo, enquanto perseguem fama e sucesso.", director: "Damien Chazelle", writers: "Damien Chazelle", isFavorite: false, isWatched: false, rating: 0))
+            }
         }
-        .padding()
+        .tint(.menta)
+        .toolbarBackground(
+            .thinMaterial,
+            for: .tabBar
+        )
     }
 }
 
